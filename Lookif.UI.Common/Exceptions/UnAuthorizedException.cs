58aa5935bfcf97695062590369327d70a1c83097
Farshad <sfarshadhn@gmail.com>
2025-04-24 21:56:00 +0330

    2.1

---
+++ Lookif.UI.Common/Exceptions/UnAuthorizedException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lookif.UI.Common.Exceptions;
+
+public class UnAuthorizedException : Exception
+{
+    public UnAuthorizedException(string message)
+    {
+        Message = message;
+    }
+    public string Message { get; set; }
+    //ToDo Set error status
+}
